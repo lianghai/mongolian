5d57871de4ca7685dfc9a6eb29468e124c344f2b
Liang Hai <lianghai@gmail.com>
2020-09-25 02:56:47 +0800

    Update ReferenceShaper

--- draft-utn/ReferenceShaper/ReferenceShaper/main.swift
+++ draft-utn/ReferenceShaper/ReferenceShaper/main.swift
@@ -1,11 +1,190 @@
-//
-//  main.swift
-//  ReferenceShaper
-//
-//  Created by 梁海 on 9/24/20.
-//
-
 import Foundation
+import Yams
 
-print("Hello, World!")
+extension Unicode {
+    typealias CodePoint = UInt32
+    typealias Character = Scalar // Encoded character
+    typealias GraphemeCluster = Swift.Character
+}
 
+enum JoiningForm: String, CaseIterable {
+    case isol, `init`, medi, fina
+}
+
+func loadYaml(path: String) {
+    FileManager.default.urls(for: <#T##FileManager.SearchPathDirectory#>, in: <#T##FileManager.SearchPathDomainMask#>)
+}
+
+enum WrittenUnit: String, CaseIterable {
+    
+    case A, Aa, I, Ii, O, Ue, U, Uu,
+         N, B, P, H, Gh, G, Gg, M, L, S, Sh, T, D, Dd, Ch, J, Y, R, W,
+         F, K, C, Z, Hh, Rh, Zr, Cr
+    
+    static let dataPath = "../../data/written-units.yaml"
+    static let data = Yams.load(yaml: String(contentsOfFile: <#T##String#>))
+}
+
+typealias Variant = (joiningForm: JoiningForm, writtenUnits: [WrittenUnit])
+
+enum Condition {
+    case fallback
+    case chachlag
+}
+
+enum Character: String, CaseIterable {
+    
+    case MVS,
+         aleph,
+         a, e, ee, i, o, u, oe, ue,
+         n, ng, b, p, h, g, m, l, s, sh, t, d, ch, j, y, r, w,
+         f, k, c, z, hh, rh, lh, zr, cr
+    
+    static let data =
+}
+
+//static let data: [Character: [JoiningForm: [[WrittenUnit]: Set<Condition>]]] = [
+//    .a: [
+//        .isol: [
+//            [.A, .A]: [.fallback]
+//        ]
+//    ]
+//]
+//lazy var conditionToWrittenUnits: [Condition: [WrittenUnit]]? = {
+//    var conditionToWrittenUnits: [Condition: [WrittenUnit]] = [:]
+//    if let joiningForm = joiningForm, let writtenUnitsToConditions = Letter.data[codePoint]?[joiningForm] {
+//        for (writtenUnits, conditions) in writtenUnitsToConditions {
+//            for condition in conditions {
+//                conditionToWrittenUnits[condition] = writtenUnits
+//            }
+//        }
+//    }
+//    return conditionToWrittenUnits
+//}()
+//
+
+struct Letter { // Phonetic letter
+    
+    let character: Character
+    var joiningForm: JoiningForm?
+    var writtenUnits: [WrittenUnit]?
+    
+    init?(codePoint: Unicode.CodePoint) {
+        guard let character = Character(rawValue: codePoint) else {
+            return nil
+        }
+        self.character = character
+    }
+    mutating func applyCondition(_ condition: Condition) {
+        writtenUnits = conditionToWrittenUnits?[condition]
+    }
+}
+
+//for character in Character.allCases {
+//    print(String(Unicode.Scalar(character.rawValue)!))
+//    print(String(character.rawValue, radix: 16, uppercase: true))
+//}
+
+var letters = "ᠮᠣᠩᠭᠣᠯ".unicodeScalars.compactMap { Letter(codePoint: $0.value) }
+
+var letter = letters[0]
+
+print(letter.character.rawValue)
+
+letter.joiningForm = .isol
+
+print(letter.joiningForm ?? "unknown")
+
+letter.applyCondition(.fallback)
+
+print(letter.writtenUnits ?? "unknown")
+
+struct Context {
+    let before: [Letter]
+    let after: [Letter]
+}
+
+//func resolveVariant(for letter: Letter, in context: Context) -> Variant {
+//
+//    var condition = Condition.fallback
+//
+//    // Chachlag
+//    if  [.a, .e].contains(letter), context.before.hasSuffix(.MVS) {
+//        condition = .chachlag
+//    }
+//
+//    // Syllabic
+//    switch letter {
+//    case .o, .u, .oe, .ue:
+//        if context.before {
+//            condition = .marked
+//        }
+//    case .n, .j, .y, .w:
+//        if context.after {
+//            condition = .chachlag_onset
+//        }
+//    case .h, .g:
+//        if context.after {
+//            condition = .chachlag_onset
+//        }
+//    case .n, .d:
+//        if context.after {
+//            condition = .onset
+//        } else if context.before {
+//            condition = .devsger
+//        }
+//    case .h, .g:
+//        if context.after {
+//            condition = .masculine_onset
+//        } else if context.after {
+//            condition = .feminine
+//        }
+//    case .g:
+//        if context.before {
+//            condition = .masculine_devsger
+//        } else if context.before {
+//            condition = .feminine
+//        } else if context.before {
+//            condition = .masculine_devsger
+//        } else {
+//            condition = .feminine
+//        }
+//    default:
+//        break
+//    }
+//
+//    // Particle
+//    switch letter {
+//    case .a, .i, .u, .ue, .d:
+//        if context.before == .nnbsp {
+//            condition = .particle
+//        }
+//    case .u, .ue:
+//        if context.before {
+//            condition = .particle
+//        }
+//    case .y:
+//        if context.before, context.after {
+//            condition = .particle
+//        }
+//    default:
+//        break
+//    }
+//
+//    // Devsger i
+//    switch letter {
+//    case .i:
+//        if context.before {
+//            condition = .devsger
+//        }
+//    default:
+//        break
+//    }
+//
+//    // Post-bowed
+//    if [.o, .u, .oe, .ue].contains(letter), written_form == .U, context.before {
+//        condition = .post_bowed
+//    }
+//
+//    return condition
+//}
