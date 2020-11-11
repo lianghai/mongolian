ef0cc48d16fe4ce64319473fd57d2d9236a63897
Liang Hai <lianghai@gmail.com>
2020-11-12 03:31:39 +0800

    Populate YAML data files

--- otl-dummy-fonts/build.py
+++ otl-dummy-fonts/build.py
@@ -1,7 +1,9 @@
 from pathlib import Path
+from types import SimpleNamespace
+from typing import Any
 
+import yaml
 from tptqscripttools.data import REGISTER
-from tptqscripttools.objects import Script
 from tptqscripttools.otl import FeaFile
 
 scripting_path = Path(__file__)
@@ -10,20 +12,44 @@ project_dir = scripting_path.parent
 feature_file_path = project_dir / "stateless.fea"
 product_dir = project_dir / "products"
 
+def make_namespace(content: Any) -> Any:
+    if isinstance(content, dict):
+        return SimpleNamespace(**{k: make_namespace(v) for k, v in content.items()})
+    elif isinstance(content, list):
+        if content and isinstance(i := content[0], dict) and "ascii_transcription" in i.keys():
+            return SimpleNamespace(**{i["ascii_transcription"]: make_namespace(i) for i in content})
+        else:
+            return [make_namespace(i) for i in content]
+    else:
+        return content
+
+data_dir = project_dir / ".." / "utn" / "data"
+data_dict = {}
+for filename in ["written-units", "characters", "syllabic-category"]:
+    text = (data_dir / filename).with_suffix(".yaml").read_text()
+    data_dict[filename.replace("-", "_")] = make_namespace(yaml.safe_load(text))
+data = SimpleNamespace(**data_dict)
+
 
 def main():
+
     script = REGISTER.script_by_code["Mong"]
-    glyph_space = script.glyph_space()  # not checking availability in a source glyph set
-    build_otl_feature_file(glyph_space)
-    script.export_otl_dummy_font(project_dir / "products", feature_file_path)
-
-
-def build_otl_feature_file(glyph_space: Script):
-
-    mong = glyph_space
+    mong = script.glyph_space()  # not checking availability in a source glyph set
 
     with FeaFile(feature_file_path, source=scripting_path) as file:
-        pass
+
+        vowel_subclass_to_members = {
+            "masculine": ["a", "o", "u"],
+            "feminine": ["e", "oe", "ue", "eh"],
+            "neuter": ["i"],
+        }
+        for subclass, members in vowel_subclass_to_members.items():
+            file.classDefinition("@vowel." + subclass, ["@" + m for m in members])
+        file.classDefinition("@vowel", ["@vowel." + s for s in vowel_subclass_to_members.keys()])
+
+    return
+
+    script.export_otl_dummy_font(project_dir / "products", feature_file_path)
 
 
 if __name__ == "__main__":
--- sandbox-font/README.md
+++ sandbox-font/README.md
@@ -2,6 +2,8 @@
 
 # OFL FONTLOG: Sandbox font for the Mongolian script
 
+components/shapes.glyphs → components/written-units.ufo → variants/variants.ufo
+
 ## Glyphs
 
 The glyph **outlines** are derived from **Noto Sans Mongolian**’s source files ([googlefonts/noto-source/src/NotoSansMongolian/ at the commit 933987e](https://github.com/googlefonts/noto-source/tree/933987e2509b9ae5192420a8296f330c25df7652/src/NotoSansMongolian)), which are [archived here](https://github.com/lianghai/mongolian/tree/master/sandbox-font/references/NotoSansMongolian) for quick reference.
