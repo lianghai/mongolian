4526286f7a493b15deae7c93b2a26153cb3d6816
Liang Hai <lianghai@gmail.com>
2021-04-15 02:15:46 +0200

    Normalize glyph names

--- font-tooling/scripting/real_text_test.py
+++ font-tooling/scripting/real_text_test.py
@@ -1,5 +1,6 @@
 import difflib
 import logging
+from collections import Counter
 from pathlib import Path
 from textwrap import indent
 
@@ -19,8 +20,7 @@ corpus_dir = private_repo_dir / "misc/jirimutu/mongol_corpus"
 
 def main():
 
-    # text = "\n".join((corpus_dir / f"almas_{i:03}.txt").read_text() for i in range(1, 65))
-    text = (corpus_dir / "almas_005.txt").read_text()
+    text = "\n".join((corpus_dir / f"almas_{i:03}.txt").read_text() for i in range(1, 65))
 
     def normalize_text(text: str) -> str:
         for k, v in {
@@ -30,21 +30,27 @@ def main():
             "᠂": " ",
             "᠃": " ",
             "᠄": " ",
+            "᠁": " ",
             "«": " ",
             "»": " ",
+            "︖": " ",
+            "︕": " ",
+            ">": " ",
+            "<": " ",
+            "Q": " ",
+            "D": " ",
+            "⟫": " ",
+            "⟪": " ",
         }.items():
             text = text.replace(k, v)
         return text
 
-    cases = []
+    cases = Counter[str]()
     for line in normalize_text(text).splitlines():
-        for word in line.split(" "):
-            cases.append(word)
-
-    # cases = cases[:10]
+        cases.update(line.split(" "))
 
     passed = 0
-    for index, case in enumerate(cases):
+    for case, count in cases.most_common():
 
         eac_shaper = Shaper(font_dir / "MongolQaganTig.ttf")
         eac_names = eac_shaper.shape_text_to_glyph_names(case)
@@ -55,16 +61,17 @@ def main():
         utn_result = normalize_utn(utn_names)
 
         if eac_result == utn_result:
-            passed += 1
-        else:
+            passed += count
+        elif count > 100:
             string_notation = ", ".join(
                 cp_to_name.get(i) or unicodedata.name(i, f"U+{ord(i):04X}") for i in case
             )
-            logging.info(f" Failed case {index}: {string_notation}")
+            logging.info(f" Failed case (count {count}): {string_notation}")
             for line in [*difflib.unified_diff(eac_result, utn_result, lineterm="")][3:]:
                 print(indent(line, " " * 4))
 
-    logging.info(f" Passed cases: {passed}/{len(cases)} = {passed / len(cases) * 100} %")
+    count_sum = sum(cases.values())
+    logging.info(f" Passed cases: {passed}/{count_sum} = {passed / count_sum * 100} %")
 
 
 cp_to_name = {chr(character.cp): character_id for character_id, character in mongolian.characters.items()}
@@ -84,8 +91,6 @@ def normalize_eac(names: list[str]) -> list[str]:
     normalized = []
     for eac_name in names:
         standard_name = eac_name_to_standard.get(eac_name) or eac_name
-        # if standard_name.startswith("uni"):
-        #     raise Exception(standard_name)
         if standard_name in [
             "space",  # HarfBuzz swap zero-width nonspacing glyphs to /space
         ]:
@@ -95,7 +100,7 @@ def normalize_eac(names: list[str]) -> list[str]:
             "e.Aa.fina": "e.Aa.isol"
         }.get(standard_name) or standard_name
         normalized.extend(split_ligature(standard_name))
-    return normalized
+    return [{"K2.init": "K.init", "K2.medi": "K.medi", "K2.fina": "K.fina"}.get(i) or i for i in normalized]
 
 
 def normalize_utn(names: list[str]) -> list[str]:
@@ -105,7 +110,10 @@ def normalize_utn(names: list[str]) -> list[str]:
             continue
         if standard_name in ["_fvs1", "_fvs2", "_fvs3", "_fvs4"]:
             continue
-        normalized.extend(split_ligature(standard_name))
+        if standard_name == "_nnbsp":
+            normalized.append("nnbsp")
+        else:
+            normalized.extend(split_ligature(standard_name))
     return normalized
 
 
