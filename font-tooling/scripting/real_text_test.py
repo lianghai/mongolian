f873e6d339b6bda055071963ddc8e3e3245c63ad
Liang Hai <lianghai@gmail.com>
2021-06-10 00:21:59 +0200

    Clarify testees in reports

--- font-tooling/scripting/real_text_test.py
+++ font-tooling/scripting/real_text_test.py
@@ -1,8 +1,12 @@
 import difflib
 import re
 from collections import Counter
+from collections.abc import Callable
+from dataclasses import dataclass
+from functools import cache
 from pathlib import Path
 from textwrap import indent
+from unicodedata import normalize
 
 from fontTools import unicodedata
 from tptq.utils.shaping import Shaper
@@ -15,7 +19,19 @@ scripting_dir = Path(__file__).parent
 project_dir = scripting_dir / ".."
 repo_dir = scripting_dir / ".." / ".."
 private_repo_dir = repo_dir / ".." / "mongolian-private"
-font_dir = private_repo_dir / "misc/liangjinbao/20210303"
+
+
+@dataclass
+class Testee:
+
+    name: str
+    shaper: Shaper
+    normalizer: Callable[[list[str]], list[str]]
+
+    def shape(self, string: str) -> list[str]:
+        glyph_names = self.shaper.shape_text_to_glyph_names(string)
+        return self.normalizer(glyph_names)
+
 
 corpus_loader_by_tag = {
     "combined": (
@@ -32,18 +48,38 @@ corpus_loader_by_tag = {
     ),
 }
 
+
 def main():
 
-    tag = "combined"
-    text = corpus_loader_by_tag[tag]()
-
-    cases = Counter[str](
-        i.group(0) for i in
-        re.finditer(r"\u202F?[\u200C\u200D\u180A-\u180E\u1807\u1820-\u1842]+", text)
+    microsoft = Testee(
+        name="microsoft",
+        shaper=Shaper(private_repo_dir / "misc/gregeck/20210527/monbaiti.ttf"),
+        normalizer=microsoft_normalizer,
     )
-    total = sum(cases.values())
+    eac = Testee(
+        name="eac",
+        shaper=Shaper(private_repo_dir / "misc/liangjinbao/20210303/MongolQaganTig.ttf"),
+        normalizer=eac_normalizer,
+    )
+    utn = Testee(
+        name="utn",
+        shaper=Shaper(project_dir / "products" / "DummyStateless-Regular.otf"),
+        normalizer=utn_normalizer,
+    )
 
-    with (scripting_dir / ".." / "reports" / f"{tag}.txt").open("w") as f:
+    baseline_testee = eac
+    alt_testee = utn
+    corpus_tag = "badamsuren"
+
+    filename = f"{baseline_testee.name}-vs-{alt_testee.name}-with-{corpus_tag}-corpus.txt"
+    with (scripting_dir / ".." / "reports" / filename).open("w") as f:
+
+        text = corpus_loader_by_tag[corpus_tag]()
+        cases = Counter[str](
+            i.group(0) for i in
+            re.finditer(r"\u202F?[\u200C\u200D\u180A-\u180E\u1807\u1820-\u1842]+", text)
+        )
+        total = sum(cases.values())
 
         message = f"Total word count: {total}"
         print(message)
@@ -52,15 +88,10 @@ def main():
         passed = 0
         for index, (case, count) in enumerate(cases.most_common()):
 
-            eac_shaper = Shaper(font_dir / "MongolQaganTig.ttf")
-            eac_names = eac_shaper.shape_text_to_glyph_names(case)
-            eac_result = normalize_eac(eac_names)
+            baseline = baseline_testee.shape(case)
+            alt = alt_testee.shape(case)
 
-            utn_shaper = Shaper(project_dir / "products" / "DummyStateless-Regular.otf")
-            utn_names = utn_shaper.shape_text_to_glyph_names(case)
-            utn_result = normalize_utn(utn_names)
-
-            if eac_result == utn_result:
+            if baseline == alt:
                 passed += count
             else:
                 string_notation = ", ".join(
@@ -71,7 +102,7 @@ def main():
                     message = f"Failed: case {index} (word count {count} ≈ {percentage} %): {string_notation}"
                     print(message)
                     print(message, file=f)
-                    for line in [*difflib.unified_diff(eac_result, utn_result, lineterm="")][3:]:
+                    for line in [*difflib.unified_diff(baseline, alt, lineterm="")][3:]:
                         print(indent(line, " " * 4), file=f)
                 else:
                     print(f"Failed: case {index} (word count {count} < 0.01 %): {string_notation}", file=f)
@@ -98,7 +129,11 @@ def split_ligature(name: str) -> list[str]:
     return [".".join(i) for i in zip(graphic_parts, joining_forms)]
 
 
-def normalize_eac(names: list[str]) -> list[str]:
+def microsoft_normalizer(names: list[str]) -> list[str]:
+    return names
+
+
+def eac_normalizer(names: list[str]) -> list[str]:
     normalized = []
     for eac_name in names:
         standard_name = eac_name_to_standard.get(eac_name) or eac_name
@@ -114,7 +149,7 @@ def normalize_eac(names: list[str]) -> list[str]:
     return [{"K2.init": "K.init", "K2.medi": "K.medi", "K2.fina": "K.fina"}.get(i) or i for i in normalized]
 
 
-def normalize_utn(names: list[str]) -> list[str]:
+def utn_normalizer(names: list[str]) -> list[str]:
     normalized = []
     for standard_name in names:
         if standard_name in ["_nil"]:
