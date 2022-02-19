e5422211adf44b86874996ab866774d7e58be2b8
Liang Hai <lianghai@gmail.com>
2022-02-19 22:26:37 +0100

    Clean up utn-data/

--- utn-data/scripting/dump-json.py
+++
@@ -1,9 +0,0 @@
-import json
-
-import yaml
-
-for filename in ["written-units", "characters"]:
-    with open(filename + ".yaml") as f:
-        data = yaml.safe_load(f)
-    with open(filename + ".json", "w") as f:
-        json.dump(data, f, ensure_ascii=False)
